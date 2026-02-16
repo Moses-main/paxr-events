39f18ec42b99d4a59b99e11834a4fd245e9ded5d
Moses <moseschizaram8@gmail.com>
2026-02-16 02:16:02 +0100

    revert: return to Space Grotesk font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        display: ["Allura", "cursive"],
+        display: ["Space Grotesk", "sans-serif"],
         body: ["Inter", "sans-serif"],
       },
       colors: {
